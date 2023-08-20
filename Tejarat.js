13a2118141aeedc2c94d9d73bd8c6d37c41ad78a
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:59:37 +0330

    Add files via upload

---
+++ Tejarat.js
@@ -0,0 +1,10 @@
+tejarat={
+"type": "FeatureCollection",
+"name": "Tejarat",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 10, "bank": "بانک تجارت", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.53447792251, 35.742920995782939 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "بانک تجارت", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.524341527678125, 35.732115575344302 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "بانک تجارت", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.545828899825885, 35.732240249953769 ] } }
+]
+}
