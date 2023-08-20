73636099fdf57ce5d7bc9e886aac67b014413be2
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 18:02:01 +0330

    Add files via upload

---
+++ data/Tourist.js
@@ -0,0 +1,8 @@
+tourist={
+"type": "FeatureCollection",
+"name": "Tourist",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "bank": "tourist", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.526794510535375, 35.735561294050449 ] } }
+]
+}
