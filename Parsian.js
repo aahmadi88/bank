c932d16520693f47caf0c94d29e1d81bb61278e7
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:56:14 +0330

    Add files via upload

---
+++ Parsian.js
@@ -0,0 +1,11 @@
+parsian={
+"type": "FeatureCollection",
+"name": "Parsian",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 2, "bank": "parsian" }, "geometry": { "type": "Point", "coordinates": [ 51.537409606160153, 35.750094782365451 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "parsian" }, "geometry": { "type": "Point", "coordinates": [ 51.528175049501485, 35.732707402788932 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "parsian" }, "geometry": { "type": "Point", "coordinates": [ 51.519179505537231, 35.732308274153119 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "parsian" }, "geometry": { "type": "Point", "coordinates": [ 51.554251283367122, 35.73633822076971 ] } }
+]
+}
