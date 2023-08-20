833547e3d31dbfb9f6dd3d71ba45d02e3a91eceb
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:54:33 +0330

    Add files via upload

---
+++ data/Enovin.js
@@ -0,0 +1,10 @@
+Enovin={
+"type": "FeatureCollection",
+"name": "Enovin",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "bank": "Enovin" }, "geometry": { "type": "Point", "coordinates": [ 51.529066805970828, 35.734494082129949 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "Enovin" }, "geometry": { "type": "Point", "coordinates": [ 51.518987086838543, 35.732339699707204 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "Enovin" }, "geometry": { "type": "Point", "coordinates": [ 51.543052902088206, 35.743651345939114 ] } }
+]
+}
