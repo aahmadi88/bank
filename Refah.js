b33ee21014549222264eabe2f4ee3d177f608eb5
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:57:13 +0330

    Add files via upload

---
+++ Refah.js
@@ -0,0 +1,9 @@
+refah={
+"type": "FeatureCollection",
+"name": "Refah",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "bank": "refah" }, "geometry": { "type": "Point", "coordinates": [ 51.526877654226539, 35.730270130222422 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "refah" }, "geometry": { "type": "Point", "coordinates": [ 51.527094141482316, 35.735221392507881 ] } }
+]
+}
---
+++ Saderat.js
@@ -0,0 +1,12 @@
+saderat={
+"type": "FeatureCollection",
+"name": "Saderat",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 3, "bank": "saderat" }, "geometry": { "type": "Point", "coordinates": [ 51.536760790417844, 35.748863667398723 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "saderat" }, "geometry": { "type": "Point", "coordinates": [ 51.525789288877839, 35.727194608752569 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "saderat" }, "geometry": { "type": "Point", "coordinates": [ 51.521031387000797, 35.731712156843123 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "saderat" }, "geometry": { "type": "Point", "coordinates": [ 51.548199623175236, 35.741129384014421 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "saderat" }, "geometry": { "type": "Point", "coordinates": [ 51.561600487263931, 35.740780319921285 ] } }
+]
+}
