7be8b4cae84749af3a72d030e6835a909b2abf12
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 18:05:48 +0330

    Delete Taavon.js

--- Taavon.js
+++
@@ -1,8 +0,0 @@
-taavon={
-"type": "FeatureCollection",
-"name": "Taavon",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": null, "bank": "Taavon", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.533005344276127, 35.741765414555928 ] } }
-]
-}
