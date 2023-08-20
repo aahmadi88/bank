4802c98ffa778695a8d60b1226af235cd7dc679b
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:58:45 +0330

    Add files via upload

---
+++ data/Saman.js
@@ -0,0 +1,9 @@
+saman={
+"type": "FeatureCollection",
+"name": "Saman",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 6, "bank": "saman" }, "geometry": { "type": "Point", "coordinates": [ 51.536624729118259, 35.747977009650015 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "saman" }, "geometry": { "type": "Point", "coordinates": [ 51.525464613680299, 35.727653259155318 ] } }
+]
+}
