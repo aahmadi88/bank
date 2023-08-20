6c0cea31f054478b382c3e51881a565ae97126c2
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 18:00:53 +0330

    Add files via upload

---
+++ data/Sina.js
@@ -0,0 +1,11 @@
+sina={
+"type": "FeatureCollection",
+"name": "Sina",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "bank": "sina", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.527027623650255, 35.730504718410316 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "sina", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.520242374230037, 35.727773176297433 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "sina", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.544905506307508, 35.741864359445024 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "sina", "ATM": null }, "geometry": { "type": "Point", "coordinates": [ 51.561074554519436, 35.740286789384712 ] } }
+]
+}
