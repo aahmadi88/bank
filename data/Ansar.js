ff177575036e6dce2290c317c923e53cd44f422d
aahmadi88 <130646560+aahmadi88@users.noreply.github.com>
2023-08-20 17:51:07 +0330

    Add files via upload

---
+++ data/Ansar.js
@@ -0,0 +1,8 @@
+ansar={
+"type": "FeatureCollection",
+"name": "Ansar",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "bank": "ansar" }, "geometry": { "type": "Point", "coordinates": [ 51.519941727259258, 35.732190759855634 ] } }
+]
+}
---
+++ data/Ayandeh.js
@@ -0,0 +1,10 @@
+ayandeh={
+"type": "FeatureCollection",
+"name": "Ayandeh",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 11, "bank": "Ayandeh" }, "geometry": { "type": "Point", "coordinates": [ 51.533116780427946, 35.741968380056854 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "ayandeh" }, "geometry": { "type": "Point", "coordinates": [ 51.524584387769977, 35.726679064808195 ] } },
+{ "type": "Feature", "properties": { "id": null, "bank": "ayandeh" }, "geometry": { "type": "Point", "coordinates": [ 51.520682315015613, 35.730607472449925 ] } }
+]
+}
